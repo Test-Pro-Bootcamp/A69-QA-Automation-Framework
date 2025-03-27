150c8c8d3e6ac869e0a23f29e292b58dacb3aff6
chartemisia@gmail.com <artemisia.chalkiopoulou@testpro.io>
2025-03-27 19:48:16 -0400

    Homework 17 Attempt 3

--- src/test/java/Homework17.java
+++ src/test/java/Homework17.java
@@ -1,3 +1,4 @@
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
@@ -10,39 +11,45 @@ import java.time.Duration;
 public class Homework17 {
     @Test
 
-    public void addSongToPlaylist() {
+    public void addSongToPlaylist() throws InterruptedException {
+
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
         ChromeDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         String url = "https://qa.koel.app/";
-        driver.get(url);
-        Assert.assertEquals(driver.getCurrentUrl(), url);
-        driver.navigate().to("https://qa.koel.app/");
-        Assert.assertEquals(driver.getCurrentUrl(), "https://qa.koel.app/");
-        WebElement email = driver.findElement(By.cssSelector("[type='email']"));
-        email.sendKeys("artemisia.chalkiopoulou@testpro.io");
+        driver.navigate().to(url);
+        Assert.assertTrue(driver.getPageSource().contains(url));
+        Thread.sleep(3000);
+
+        WebElement provideEmail = driver.findElement(By.cssSelector("[type='email']"));
+        provideEmail.sendKeys("artemisia.chalkiopoulou@testpro.io");
         WebElement password = driver.findElement(By.cssSelector("[type='password']"));
         password.sendKeys("22002255");
         WebElement login = driver.findElement(By.cssSelector("[type='submit']"));
         login.click();
+        Thread.sleep(3000);
 
         WebElement search = driver.findElement(By.cssSelector("[type='search']"));
         search.sendKeys("century");
-        WebElement ViewAllButton = driver.findElement(By.cssSelector("button[data-test=/'view-all-songs-btn'/]"));
-        ViewAllButton.click();
-        WebElement songResult = driver.findElement(By.xpath("//*[@id='songResultsWrapper']/div/div/div[1]/table/tr)"));
-        songResult.click();
-        WebElement addTo = driver.findElement(By.xpath("//*[@id=/'songResultsWrapper/']/header/div[3]/span/button[2]"));
-        addTo.click();
-        WebElement favoritesList = driver.findElement(By.xpath("//*[@id=/'songResultsWrapper/']/header/div[3]/div/section[1]/ul/li[4]"));
-        favoritesList.click();
-        WebElement notification = driver.findElement(By.xpath("//*[@id=/'songResultsWrapper/']/div/div/div[1]/table/tr/td[6]"));
-        String actualMessage = notification.getText();
-        String playlistName="";
-        String expectedMessage = "Added 1 song into " + playlistName;
-        Assert.assertEquals(actualMessage, expectedMessage, "Notification message does not match!");
+        Thread.sleep(3000);
 
+        WebElement ViewAllButton = driver.findElement(By.cssSelector("button[data-test='view-all-songs-btn']"));
+        ViewAllButton.click();
+        Thread.sleep(3000);
+
+        WebElement firstSong = driver.findElement(By.xpath("//td[normalize-space()='Philip Ravenel - The Greatest Century']"));
+        firstSong.click();
+        Thread.sleep(3000);
+
+        WebElement addTo = driver.findElement(By.cssSelector("button[title='Add selected songs to…']"));
+        addTo.click();
+        Thread.sleep(3000);
+
+        WebElement mySongsPlaylist = driver.findElement(By.xpath("//section[@id='songResultsWrapper']//li[@class='playlist'][normalize-space()='My Songs']"));
+        mySongsPlaylist.click();
+        Thread.sleep(3000);
+// There is no message when I add a song to either lists. This is why I didn't include an "assert" method.
         driver.quit();
 
     }
