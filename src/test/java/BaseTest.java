bed847f823fc25fa466d275f6fe1be3683334375
ArtemisCT <artemisia.chalkiopoulou@testpro.io>
2025-04-15 16:13:57 -0400

    Homework 24 _ Attempt 1

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -4,28 +4,44 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.interactions.Actions;
+import org.openqa.selenium.remote.CapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.safari.SafariDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.*;
 
+import java.net.MalformedURLException;
+import java.net.URI;
 import java.time.Duration;
 
 public class BaseTest {
-    public WebDriver driver;
+    public static WebDriver driver;
     public WebDriverWait wait;
     public Actions actions;
     public String url = "https://qa.koel.app/";
 
+    @BeforeSuite
+    static void setupClass(){
+        WebDriverManager.chromedriver().setup();
+    }
+
     @BeforeMethod
     @Parameters({"BaseURL"})
-    public void launchBrowswer(String BaseURL) {
+    public void launchBrowswer(String BaseURL) throws MalformedURLException {
         WebDriverManager.chromedriver().setup();
 
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
+        options.addArguments("--disable-notifications");
+        options.addArguments("--start-maximized");
 
-        driver = new ChromeDriver(options);
+//        driver = new ChromeDriver(options);
+        driver = pickBrowser(System.getProperty("browser"));
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         driver.manage().window().maximize();
 
@@ -34,13 +50,13 @@ public class BaseTest {
 
         driver.get(url);
         url = BaseURL;
-
     }
+
 
     public void provideEmail(String email) {
         WebElement emailField = driver.findElement(By.cssSelector("input[type='email']"));
         emailField.clear();
-        emailField.sendKeys (email);
+        emailField.sendKeys(email);
     }
 
     public void providePassword(String pass) {
@@ -65,8 +81,50 @@ public class BaseTest {
                 By.xpath("//li[contains(@class,'playlist')]//span[text()='" + name + "']")));
     }
 
-    @AfterMethod
-    public void closeBrowser() {
+    @AfterMethod(alwaysRun = true)
+    public void tearDown() {
+
         driver.quit();
     }
+
+    public void openUrl(String url) {
+        driver.get(url);
+    }
+
+    public static WebDriver pickBrowser(String browser) throws MalformedURLException {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        String gridURL = "http://192.168.1.166:4444";
+
+        switch (browser) {
+            case "firefox":
+                WebDriverManager.firefoxdriver().setup();
+                return driver = new FirefoxDriver();
+
+                case "safari":
+                WebDriverManager.safaridriver().setup();
+                return driver = new SafariDriver();
+
+                case "edge":
+                WebDriverManager.edgedriver().setup();
+                return driver = new EdgeDriver();
+
+                case "grid-firefox":
+                capabilities.setCapability("browserName", "firefox");
+                return driver = new RemoteWebDriver(URI.create(gridURL), toURL(), capabilities);
+
+                case "grid-safari":
+                capabilities.setCapability("browserName", "safari");
+                return driver = new RemoteWebDriver(URI.create(gridURL), toURL(), capabilities);
+
+                default:
+                WebDriverManager.chromedriver().setup();
+                ChromeOptions options = new ChromeOptions();
+                options.addArguments("--remote-allow-origins=*");
+                options.addArguments("--disable-notifications");
+                options.addArguments("--start-maximized");
+                return driver = new ChromeDriver(options);
+
+        }
+    }
+
 }
